f068177e4ce9db3a1b35de46f08bcfd44312383a
Chhotu Kumar <chhotukumar@Chhotus-MacBook-Air.local>
2023-10-15 02:25:07 +0530

    initial comit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
 module.exports = {
-  content: ["./dist/**/*.html"],
+  content: ["./public/**/*.html"],
   theme: {
     extend: {
       fontFamily:{
